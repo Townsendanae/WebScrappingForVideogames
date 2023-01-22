f82923c5d7b903f2e0316627ebbd73210b4969a4
Danae Townsend <danjtown@espol.edu.ec>
2023-01-21 22:32:03 -0500

    Frontend - Economy Games

--- angular-13/src/app/dashboard/dashboard-components/blog-cards/blog-cards-data.ts
+++ angular-13/src/app/dashboard/dashboard-components/blog-cards/blog-cards-data.ts
@@ -1,39 +1,143 @@
 export interface blogcard {
     title: string,
-    subtitle: string,
-    subtext: string,
-    image: string
+    price: string,
+    discount: string,
+    pictureLink: string,
+    linkToShop: string,
+    shopName: string
 }
 
 
+// fetch('./prueba.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         // data contiene la información del archivo JSON
+//         console.log(data);
+//         // Aqui puedes utilizar la data para tu aplicacion
+//     })
+//     .catch(error => console.error(error));
 
 
 
 export const blogcards: blogcard[] = [
-
     {
-        title: 'This is simple blog',
-        subtitle: '2 comments, 1 Like',
-        subtext: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-        image: 'assets/images/bg/bg1.jpg'
+        "title": "Heavenly Heroes of Antidomi UNRATED",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://images.gog-statics.com/6bdba080e0d01f61b80627a918ed43e7a3c9d9a2998a9d4524fd1f290e3ef1da_product_tile_extended_432x243.webp",
+        "linkToShop": "https://www.gog.com/en/game/heavenly_heroes_of_antidomi_unrated",
+        "shopName": "Gog"
     },
     {
-        title: 'This is simple blog',
-        subtitle: '2 comments, 1 Like',
-        subtext: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-        image: 'assets/images/bg/bg2.jpg'
+        "title": "Way of the Hunter - Free UTV DLC",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://images.gog-statics.com/82fae0323f0798d17e66ae06b8fb5d92ed5654d50fb27e152f4438801eb4575b_product_tile_extended_432x243.webp",
+        "linkToShop": "https://www.gog.com/en/game/way_of_the_hunter_free_utv_dlc",
+        "shopName": "Gog"
     },
     {
-        title: 'This is simple blog',
-        subtitle: '2 comments, 1 Like',
-        subtext: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-        image: 'assets/images/bg/bg3.jpg'
+        "title": "UnMetal (PC) Steam Key GLOBAL",
+        "price": "0.99",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/x146Fa46daXngCPgUtx_-RaFuhzWoMtTlVt44xbuhpI_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-unmetal-pc-steam-key-global",
+        "shopName": "Ebena"
     },
     {
-        title: 'This is simple blog',
-        subtitle: '2 comments, 1 Like',
-        subtext: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-        image: 'assets/images/bg/bg4.jpg'
+        "title": "Tunguska The Visitation - Ghost of Valentin",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://images.gog-statics.com/2ead5c184228f5249c85bb9bc42c4ca800a32df87b62b4ae2e6ab2497e942315_product_tile_extended_432x243.webp",
+        "linkToShop": "https://www.gog.com/en/game/tunguska_the_visitation_ghost_of_valentin",
+        "shopName": "Gog"
     },
-
+    {
+        "title": "Street Football Steam Key GLOBAL",
+        "price": "0.72",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/yI0jC9r3kGXeq5J111F4Lal7vbskMW7snSG1YJ_OBPM_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-street-football-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "Nyasha (PC) Steam Key GLOBAL",
+        "price": "0.49",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/Xizsm1WUfFgOwyGeVeLJYRnxRJT5Gte3r1hRjj4-I5E_350x200_1x-0.jpg",
+        "linkToShop": "https://www.eneba.com/latam/steam-nyasha-pc-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "X-COM: Complete Pack (PC) Steam Key GLOBAL",
+        "price": "0.28",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/W3Dltbe_350x200_1x-0.jpg",
+        "linkToShop": "https://www.eneba.com/latam/steam-x-com-complete-pack-pc-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "Destroy All Humans! Clone Carnage",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2020/02/17105257/game-ratings-featured.jpg",
+        "linkToShop": "https://www.gog.com/en/game/destroy_all_humans_clone_carnage",
+        "shopName": "Gog"
+    },
+    {
+        "title": "Neverout Steam Key GLOBAL",
+        "price": "0.40",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/JyZE3g-MTqAG4geh_D6Qxf_W4CFdQ64pjg7TOSiLcyw_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-neverout-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "Sherlock Holmes Chapter One - Clergyman Cassock",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2020/02/17105257/game-ratings-featured.jpg",
+        "linkToShop": "https://www.gog.com/en/game/sherlock_holmes_chapter_one_clergyman_cassock",
+        "shopName": "Gog"
+    },
+    {
+        "title": "The Evil Within 2 (PC) Gog.com  Key GLOBAL",
+        "price": "0.37",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/z9ooqscxnvftgbpktcgt_350x200_1x-0.jpg",
+        "linkToShop": "https://www.eneba.com/latam/gog-the-evil-within-2-pc-gog-com-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "LEGO: Batman 2 - DC Super Heroes Steam Key GLOBAL",
+        "price": "0.95",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/5WFo3QeDVAV1Tvgt-0EMoEhlQkDe5elKlWFspehsKTI_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-lego-batman-2-dc-super-heroes-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "Resident Evil: Revelations 2 Episode One: Penal Colony Steam Key GLOBAL",
+        "price": "0.36",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/w3o6CROMZAMVe-1t9H6s_imiOyD7Ip_ouZZoZm2rAp4_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-resident-evil-revelations-2-episode-one-penal-colony-steam-key-global",
+        "shopName": "Ebena"
+    },
+    {
+        "title": "Gamedec - Definitive Edition - Free Characters Bundle",
+        "price": "0.00",
+        "discount": "0",
+        "pictureLink": "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2020/02/17105257/game-ratings-featured.jpg",
+        "linkToShop": "https://www.gog.com/en/game/gamedec_definitive_edition_free_characters_bundle",
+        "shopName": "Gog"
+    },
+    {
+        "title": "Omno Steam Key GLOBAL",
+        "price": "0.80",
+        "discount": "\"\"",
+        "pictureLink": "https://cdn-products.eneba.com/resized-products/N2jRIUAOmxMjqS2_FFxXeCdicRmLeoWZAC-aHB_POW8_350x200_1x-0.jpeg",
+        "linkToShop": "https://www.eneba.com/latam/steam-omno-steam-key-global",
+        "shopName": "Ebena"
+    }
 ] 
