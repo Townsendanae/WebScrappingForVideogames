20e980fddc6302d88ed6d1bb29aa8f7bedcc465d
DvMarc <romdmarc@espol.edu.ec>
2023-01-30 23:32:21 -0500

    updt

--- scripting/prueba.js
+++ scripting/prueba.js
@@ -1,4 +1,15 @@
 const { execSync } = require('child_process');
-const game = "The forest";
+const express = require('express');
+const crypto = require('crypto');
 
-const rubyExec = execSync(`ruby GeneralBusqueda.rb "${game}"`);
+const app = express();
+
+app.get('/game/:idGame', (req, res) => {
+  console.log(req.params)
+  const rubyExec = execSync(`ruby GeneralBusqueda.rb "${req.params.idGame}"`);
+  res.send('Sucess');
+});
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3000');
+});
