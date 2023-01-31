112daaaf56f411384c8368ddb78119f503cb7613
Danae Townsend <danjtown@espol.edu.ec>
2023-01-31 04:16:32 -0500

    Cartitas en buscar pero supervisado por Rommel

--- angular-13/src/app/component/badge/badge.component.ts
+++ angular-13/src/app/component/badge/badge.component.ts
@@ -7,41 +7,63 @@ import { blogcard, blogcards } from "./blog-cards-data";
   templateUrl: "./badge.component.html",
 })
 
-export class BadgeComponent { 
+export class BadgeComponent {
 
   blogcards: blogcard[];
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { 
-    
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+
     this.blogcards = blogcards;
     this.route.queryParams.subscribe(params => {
       let input = params['input'];
-        let headers = new Headers();
-    
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Origin','http://localhost:3001');
-    
-        fetch("http://localhost:3001/game/"+input, {
-            mode: 'no-cors',
-            credentials: 'include',
-            method: 'GET',
-            headers: headers
-        })
+      let headers = new Headers();
+
+      headers.append('Content-Type', 'application/json');
+      headers.append('Accept', 'application/json');
+      headers.append('Origin', 'http://localhost:3001');
+
+      fetch("http://localhost:3001/game/" + input, {
+        mode: 'no-cors',
+        credentials: 'include',
+        method: 'GET',
+        headers: headers
+      })
 
       fetch('http://localhost:3000/totalJuegos')
-      .then(response => response.json())
-      .then(data => {
+        .then(response => response.json())
+        .then(data => {
           let arrayTemp;
           arrayTemp = data['result']
           this.blogcards = arrayTemp.filter((item: any) => item.title.toLowerCase().includes(input.toLowerCase()))
-          console.log(this.blogcards)
+
+
+          let html = "";
+          for (let i = 0; i < this.blogcards.length; i++) {
+            console.log("Aa")
+            html += `
+                <div class="col-md-6 col-lg-3">
+                  <div class="card">
+                    <img src="${this.blogcards[i].pictureLink}" alt="blogs" class="card-img-top" width="300" height="150" />
+                    <div class="card-body" id="informationCard">
+                    <h4 class="card-title">${ this.blogcards[i].title }</h4>
+                    <h6 class="card-subtitle">Tienda: ${ this.blogcards[i].shopName }</h6>
+                    <h6 class="card-subtitle">Precio: ${ this.blogcards[i].price }</h6>
+                    <a href="${ this.blogcards[i].linkToShop }">
+                      <button type="button" class="btn btn-primary">Ir al sitio</button>
+                    </a>
+                    </div>
+                  </div>
+                </div>`;
+
+          }
+
+          document.getElementById('row')!.innerHTML = html
         })
-    .catch(error => console.error(error))
+        .catch(error => console.error(error))
     });
-  
+
 
 
   }
-  
+
 }
