cf8af04a7c50aedc99cb7cc93970871bd13a3847
rxcp02 <rxcabrer@espol.edu.ec>
2023-01-31 03:40:08 -0500

    sorter by ronald

--- angular-13/src/app/component/alert/alert.component.ts
+++ angular-13/src/app/component/alert/alert.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { NgbNavChangeEvent } from "@ng-bootstrap/ng-bootstrap";
 import { blogcard, blogcards } from "./blog-cards-data";
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
-  selector: "app-ngbd-alert",
+  selector: "ngbd-dropdown-basic",
   templateUrl: "alert.component.html",
   styles: [
     `
@@ -17,6 +22,8 @@ import { blogcard, blogcards } from "./blog-cards-data";
 })
 export class NgbdAlertBasicComponent implements OnInit {
   blogcards: blogcard[];
+
+  // Array para hacer sorting
   enebaShop: blogcard[] = [];
   gogShop: blogcard[] = [];
   steamShop: blogcard[] = [];
@@ -34,6 +41,74 @@ export class NgbdAlertBasicComponent implements OnInit {
   tabs = [1, 2, 3, 4, 5];
   counter = this.tabs.length + 1;
   activeDynamic = 1;
+
+  orderByPriceAsc(arr: blogcard[]) {
+    arr.sort(function (a, b) {
+      if (isNaN(+a.price!) && !isNaN(+b.price!)) {
+        return -1;
+      } else if (!isNaN(+a.price!) && isNaN(+b.price!)) {
+        return 1;
+      } else if (!isNaN(+a.price!) && !isNaN(+b.price!)) {
+        return parseFloat(a.price!) > parseFloat(b.price!)
+          ? 1
+          : parseFloat(a.price!) == parseFloat(b.price!)
+          ? 0
+          : -1;
+      } else {
+        return a.price! > b.price! ? 1 : a.price! == b.price! ? 0 : -1;
+      }
+    });
+  }
+
+  orderByPriceDes(arr: blogcard[]) {
+    arr.sort(function (a, b) {
+      if (isNaN(+a.price!) && !isNaN(+b.price!)) {
+        return 1;
+      } else if (!isNaN(+a.price!) && isNaN(+b.price!)) {
+        return -1;
+      } else if (!isNaN(+a.price!) && !isNaN(+b.price!)) {
+        return parseFloat(a.price!) > parseFloat(b.price!)
+          ? -1
+          : parseFloat(a.price!) == parseFloat(b.price!)
+          ? 0
+          : 1;
+      } else {
+        return a.price! > b.price! ? -1 : a.price! == b.price! ? 0 : 1;
+      }
+    });
+  }
+
+  orderByTitleAsc(arr: blogcard[]) {
+    arr.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  orderByTitleDes(arr: blogcard[]) {
+    arr.sort((a, b) => -1 * a.title.localeCompare(b.title));
+  }
+
+  orderPArrayAsc() {
+    this.orderByPriceAsc(this.enebaShop);
+    this.orderByPriceAsc(this.gogShop);
+    this.orderByPriceAsc(this.steamShop);
+  }
+
+  orderPArrayDes() {
+    this.orderByPriceDes(this.enebaShop);
+    this.orderByPriceDes(this.gogShop);
+    this.orderByPriceDes(this.steamShop);
+  }
+
+  orderTArrayAsc() {
+    this.orderByTitleAsc(this.enebaShop);
+    this.orderByTitleAsc(this.gogShop);
+    this.orderByTitleAsc(this.steamShop);
+  }
+
+  orderTArrayDes() {
+    this.orderByTitleDes(this.enebaShop);
+    this.orderByTitleDes(this.gogShop);
+    this.orderByTitleDes(this.steamShop);
+  }
 
   onNavChange(changeEvent: NgbNavChangeEvent) {
     if (changeEvent.nextId === 3) {
@@ -66,12 +141,18 @@ export class NgbdAlertBasicComponent implements OnInit {
   ngOnInit(): void {
     let datosArray;
 
-    fetch('http://localhost:3000/totalJuegos')
-    .then(response => response.json())
-    .then(data => {
-        datosArray = data['result'];
+    fetch("http://localhost:3000/totalJuegos")
+      .then((response) => response.json())
+      .then((data) => {
+        datosArray = data["result"];
 
         for (let juego of datosArray) {
+          if (juego.price.includes("€")) {
+            juego.price = juego.price.substring(0, juego.price.length - 2);
+          }
+          if (juego.price.includes("$")) {
+            juego.price = juego.price.substring(1, juego.price.length);
+          }
           if (juego.shopName == "Eneba") {
             this.enebaShop.push(juego);
           }
@@ -84,8 +165,7 @@ export class NgbdAlertBasicComponent implements OnInit {
         }
 
         this.blogcards += datosArray;
-
-    })
-    .catch(error => console.error(error));
+      })
+      .catch((error) => console.error(error));
   }
 }
